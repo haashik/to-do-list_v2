38a4d41fd6570adb4768496812b4b7bb9e378a24
haashik <haashikph1992@gmail.com>
2026-01-11 12:23:26 +0530

    local_storage

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
+let tasks = [];
+window.onload = function () {
+    let savedTasks = localStorage.getItem("tasks");
+
+    if (savedTasks){
+        tasks = JSON.parse(savedTasks);
+        renderTasks();
+    }
+}
+
 function addTask(){
     let input = document.getElementById("taskInput")
     let taskText = input.value;
+
     if (taskText === ""){
         alert("Please enter a task");
         return;
     }
+    tasks.push({
+        text: taskText,
+        done: false
+    });
 
-    let li = document.createElement("li");
-    li.innerText = taskText
-    li.onclick = function(){
-        li.classList.toggle("done");
-    };
-    li.oncontextmenu = function() {
-        li.remove();
-        return false;
-    };
-
-    document.getElementById("taskList").appendChild(li);
+    saveTasks();
+    renderTasks();
     input.value = "";
+}
+
+// Save to localStorage
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Show tasks on screen
+function renderTasks() {
+    let list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach((task,index) => {
+        let li = document.createElement("li");
+        li.innerText = task.text;
+
+        if (task.done) {
+            li.classList.add("done");
+        }
+        li.onclick = function () {
+            task.done = !task.done; 
+            saveTasks();
+            renderTasks();
+        };
+        list.appendChild(li);
+    });
 }
